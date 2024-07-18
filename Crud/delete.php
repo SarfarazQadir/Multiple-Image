06762ff5b3e9aa675731c1e90189edbf9b282823
Sarfaraz Qadir <sarfarazqadir594@gmail.com.com>
2024-07-18 21:47:32 +0500

    delete

--- Crud/delete.php
+++ Crud/delete.php
@@ -6,5 +6,26 @@ $id = $_GET['id'];
 
 $result = $database->delete($id);
 
+if($result){
+    echo "success";
+    header('Location:fetch.php');
+    }else{
+        echo "error";
+}
+
+
+// if($result){
+//     echo"
+//     <script>alert('Delete Successfull');
+//     window.location.href:fetch.php;
+//     </script>
+//     ";
+// }else{
+//     echo"
+//     <script>alert('Delete not Successfull');
+//     </script>
+//     ";   
+// }
+
 
 ?>
